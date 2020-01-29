784de44b9f14d07175b6c427fb98b6d59b911b0e
anik511580 <engr.anikbarua@gmail.com>
2020-01-29 19:47:47 +0200

    challenge 1

---
+++ Challenge_1/url_address.py
@@ -0,0 +1,10 @@
+
+"""
+The first program from pythonchallenge.com
+
+Steps:
+    - we find the value of 2**38
+    - then paste the value http://www.pythonchallenge.com/pc/def/274877906944.html
+"""
+
+print(2**38)
